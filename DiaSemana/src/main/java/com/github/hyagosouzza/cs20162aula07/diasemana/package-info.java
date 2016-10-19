b32f534041a90b8dc7210e040a2360ffb120e9b9
hyagosouzza <hyagosouzza@hotmail.com>
2016-10-19 12:06:43 -0200

    Descrição do pacote

--- DiaSemana/src/main/java/com/github/hyagosouzza/cs20162aula07/diasemana/package-info.java
+++ DiaSemana/src/main/java/com/github/hyagosouzza/cs20162aula07/diasemana/package-info.java
@@ -1,4 +1,7 @@
 /**
  * Pacote : com.github.hyagosouzza.cs20162aula07.diasemana.
+ * Pacote que contém o métodos/algoritmos para trabalhar com duas
+ * datas a fim de encontrar um dia da semana específico(desejado).
+ * @version 1.0
  */
 package com.github.hyagosouzza.cs20162aula07.diasemana;
